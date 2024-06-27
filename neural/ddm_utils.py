48e95c113c7bac2521ecc5831d6cd42c2810b2b7
rakaar <rka87338@gmail.com>
2024-06-28 00:45:57 +0530

    ex 2.1

--- neural/ddm_utils.py
+++ neural/ddm_utils.py
@@ -2,57 +2,39 @@ import numpy as np
 from numba import jit, njit
 from scipy import integrate
 
+
 @jit(nopython=True)
-def rtd_density(t, v, a, w, K_max=100):
-    # t = t/(a**2/4)
+def rtd_density(t, mu, K_max=100):
+    k_vals = np.linspace(0, K_max, K_max+1)
+    sum_neg_1_term = (-1)**k_vals
+    sum_two_k_term = (2*k_vals) + 1
+
     if t > 0.25:
-        non_sum_term = (np.pi/a**2)*np.exp(-v*a*w - (v**2 * t/2))
-        k_vals = np.linspace(1, K_max, K_max)
-        sum_sine_term = np.sin(k_vals*np.pi*w)
-        sum_exp_term = np.exp(-(k_vals**2 * np.pi**2 * t)/(2*a**2))
-        sum_result = np.sum(k_vals * sum_sine_term * sum_exp_term)
+        non_sum_term = np.pi/2 * np.cosh(mu) * np.exp(-(mu**2)*t/2)
+        sum_exp_term = np.exp(-(((2*k_vals + 1)**2) * (np.pi**2) * t)/8)
     else:
-        non_sum_term = (1/a**2)*(a**3/np.sqrt(2*np.pi*t**3))*np.exp(-v*a*w - (v**2 * t)/2)
-        K_max = int(K_max/2)
-        k_vals = np.linspace(-K_max, K_max, 2*K_max + 1)
-        sum_w_term = w + 2*k_vals
-        sum_exp_term = np.exp(-(a**2 * (w + 2*k_vals)**2)/(2*t))
-        sum_result = np.sum(sum_w_term*sum_exp_term)
+        non_sum_term = 2*np.cosh(mu)*np.exp(-(mu**2)*t/2)*(1/np.sqrt(2 * np.pi * t**3))
+        sum_exp_term = np.exp(-((2*k_vals + 1)**2)/(2*t))
 
-    return non_sum_term * sum_result
+    sum_term = np.sum(sum_neg_1_term*sum_two_k_term*sum_exp_term)
 
+    return non_sum_term*sum_term
+        
 
-# Using Loops for confirmation
-# @jit(nopython=True)
-# def rtd_density(t, v, a, w, K_max=100):
-#     if t > 0.25:
-#         non_sum_term = (np.pi / a**2) * np.exp(-v * a * w - (v**2 * t / 2))
-#         sum_result = 0.0
-#         for k in range(1, K_max + 1):
-#             sine_term = np.sin(k * np.pi * w)
-#             exp_term = np.exp(-(k**2 * np.pi**2 * t) / (2 * a**2))
-#             sum_result += k * sine_term * exp_term
-#     else:
-#         non_sum_term = (1 / a**2) * (a**3 / np.sqrt(2 * np.pi * t**3)) * np.exp(-v * a * w - (v**2 * t) / 2)
-#         K_max = int(K_max / 2)
-#         sum_result = 0.0
-#         for k in range(-K_max, K_max + 1):
-#             w_term = w + 2 * k
-#             exp_term = np.exp(-(a**2 * (w_term)**2) / (2 * t))
-#             sum_result += w_term * exp_term
-
-#     return non_sum_term * sum_result
-
-
-
-
-def prob_rt(t_arr, v, a, w, K_max=100):
+def prob_rt(t_arr, v):
     N_t = len(t_arr)
     prob_arr = np.zeros((N_t-1,1))
     for i in range(0, N_t-1):
-        prob_arr[i] = integrate.quad(rtd_density, t_arr[i], t_arr[i+1], args=(v,a,w,K_max))[0]
-
+        prob_arr[i] = integrate.quad(rtd_density, t_arr[i], t_arr[i+1], args=(v))[0]
     return prob_arr
+
+
+# def prob_rt(t_arr, v, a, w, K_max=100):
+#     N_t = len(t_arr)
+#     prob_arr = np.zeros((N_t-1,1))
+#     for i in range(0, N_t-1):
+#         prob_arr[i] = integrate.quad(rtd_density, t_arr[i], t_arr[i+1], args=(v,a,w,K_max))[0]
+#     return prob_arr
 
 
 def calculate_histogram(x_axis, y_axis):
@@ -75,7 +57,39 @@ def simulate_ddm(v, a, dt=1e-5):
         DV += v*dt + np.random.normal(0, dB)
         t += 1
 
-        if DV >= a:
+        if DV >= a/2:
             return +1, t*dt
-        elif DV <= -a:
+        elif DV <= -a/2:
             return -1, t*dt
+        
+
+@jit(nopython=True)
+def rtd_density_a(t, v, a, w, K_max=100):
+    if t > 0.25:
+        non_sum_term = (np.pi/a**2)*np.exp(-v*a*w - (v**2 * t/2))
+        k_vals = np.linspace(1, K_max, K_max)
+        sum_sine_term = np.sin(k_vals*np.pi*w)
+        sum_exp_term = np.exp(-(k_vals**2 * np.pi**2 * t)/(2*a**2))
+        sum_result = np.sum(k_vals * sum_sine_term * sum_exp_term)
+    else:
+        non_sum_term = (1/a**2)*(a**3/np.sqrt(2*np.pi*t**3))*np.exp(-v*a*w - (v**2 * t)/2)
+        K_max = int(K_max/2)
+        k_vals = np.linspace(-K_max, K_max, 2*K_max + 1)
+        sum_w_term = w + 2*k_vals
+        sum_exp_term = np.exp(-(a**2 * (w + 2*k_vals)**2)/(2*t))
+        sum_result = np.sum(sum_w_term*sum_exp_term)
+
+    return non_sum_term * sum_result
+
+def prob_rt_a(t_arr,v,a,w):
+    N_t = len(t_arr)
+    prob_arr = np.zeros((N_t-1,1))
+    for i in range(0, N_t-1):
+        prob_arr[i] = integrate.quad(rtd_density_a, t_arr[i], t_arr[i+1], args=(v,a,w))[0]
+    return prob_arr
+
+def prob_hit_low_bound(v,a,w):
+    if v == 0:
+        return 1 - w
+    else:
+        return (1 - np.exp(-2*v*a*(1-w)))/(np.exp(2*v*w*a) - np.exp(-2*v*a*(1-w)))
